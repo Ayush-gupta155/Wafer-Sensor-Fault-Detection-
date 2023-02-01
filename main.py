ca4e14463662d74f366b86bfc72da57263b2eca3
Ayush Gupta <agayushgupta12@gmail.com>
2023-02-01 22:50:33 +0530

    first commit

--- main.py
+++ main.py
@@ -118,10 +118,12 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successfull!!")
 
-port = int(os.getenv("PORT",5000))
-if __name__ == "__main__":
-    host = '0.0.0.0'
-    #port = 5000
-    httpd = simple_server.make_server(host, port, app)
-    # print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+# port = int(os.getenv("PORT",5000))
+# if __name__ == "__main__":
+#     host = '0.0.0.0'
+#     #port = 5000
+#     httpd = simple_server.make_server(host, port, app)
+#     # print("Serving on %s %d" % (host, port))
+#     httpd.serve_forever()
+
+app.run(debug=True)
